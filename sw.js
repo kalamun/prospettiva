62cbb46b78d350fd9c24c9b3cf62906f9f0473fa
kalamun <rp@kalamun.net>
2024-01-15 11:24:01 +0000

    Offline support

---
+++ sw.js
@@ -0,0 +1,32 @@
+const cacheName = "prospettiva";
+
+const appShellFiles = [
+  "js/controls.js",
+  "js/prospettiva.js",
+  "controls.html",
+  "index.html",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      await cache.addAll(appShellFiles);
+    })(),
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      cache.put(e.request, response.clone());
+      return response;
+    })(),
+  );
+});
